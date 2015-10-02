73c59d2713a999efe612755f8da32c3f57963157
Francis Bato <batofdb@gmail.com>
2015-10-01 20:48:57 -0700

    updated logic to include tap gestures, tags, and sets for win logic

--- TicTacToe/GameViewController.m
+++ TicTacToe/GameViewController.m
@@ -22,7 +22,11 @@
 @property int currentLocation;
 @property BOOL isValidMark;
 @property BOOL isPlayerOne;
+@property NSString *mark;
 @property NSArray *gridLayout;
+@property NSSet *answerKey;
+@property NSMutableSet *playerOneMoves;
+@property NSMutableSet *playerTwoMoves;
 @property BOOL isWinner;
 
 @end
@@ -30,8 +34,14 @@
 @implementation GameViewController
 
 - (void)viewDidLoad {
+
     [super viewDidLoad];
     self.gridLayout = [NSArray arrayWithObjects:self.topLeft,self.topMiddle,self.topRight,self.middleLeft,self.middleMiddle,self.middleRight,self.bottomLeft,self.bottomMiddle,self.bottomRight,nil];
+
+    self.answerKey = [NSSet setWithObjects:[NSSet setWithObjects:@0,@1,@2,nil],[NSSet setWithObjects:@3,@4,@5,nil],[NSSet setWithObjects:@6,@7,@8,nil],[NSSet setWithObjects:@0,@3,@6,nil],[NSSet setWithObjects:@1,@4,@7,nil],[NSSet setWithObjects:@2,@5,@8,nil],[NSSet setWithObjects:@0,@4,@8,nil],[NSSet setWithObjects:@2,@5,@6,nil],nil];
+
+    self.playerOneMoves = [[NSMutableSet alloc] init];
+    self.playerTwoMoves = [[NSMutableSet alloc] init];
 
     self.isWinner = NO;
     self.isPlayerOne = YES;
@@ -40,112 +50,48 @@
 
 }
 
-
-
 - (void) updateGameBoard {
+
     if (self.isPlayerOne){
         self.gameIndicator.text = @"X";
     } else {
         self.gameIndicator.text = @"O";
     }
-
-    [self checkForWinner];
-
-    if (self.isWinner == YES){
-
-        NSString *playerString = @"Player 1";
-        if (self.isPlayerOne)
-            playerString = @"Player 2";
-
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"You're a Winner!" message:[playerString stringByAppendingString:@" won!"] preferredStyle:UIAlertControllerStyleAlert];
-
-        UIAlertAction *okButton = [UIAlertAction actionWithTitle:@"YESSS!" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        [self resetAction];
-        }];
-
-        [alert addAction:okButton];
-        [self presentViewController:alert animated:YES completion:nil];
-    }
-
 }
 
 - (IBAction)tapHandler:(UITapGestureRecognizer *)sender {
 
-    CGPoint currentPoint = [sender locationInView:self.view];
-    NSLog(@"%f,%f",currentPoint.x,currentPoint.y);
-    [self findLabelUsingPoint:currentPoint];
+    UIView *currentView = (UIView *)sender.view;
+    self.currentLocation = (int) currentView.tag;
     [self markPlayerTap];
     [self updateGameBoard];
 }
 
-
-- (void) findLabelUsingPoint:(CGPoint) point {
-    if (CGRectContainsPoint(self.topLeft.frame, point)){
-
-        self.currentLocation = 0;
-
-    } else if(CGRectContainsPoint(self.topMiddle.frame, point)){
-
-        self.currentLocation = 1;
-
-    } else if(CGRectContainsPoint(self.topRight.frame, point)){
-
-        self.currentLocation = 2;
-
-    } else if(CGRectContainsPoint(self.middleLeft.frame, point)){
-
-        self.currentLocation = 3;
-
-    } else if(CGRectContainsPoint(self.middleMiddle.frame, point)){
-
-        self.currentLocation = 4;
-
-    } else if(CGRectContainsPoint(self.middleRight.frame, point)){
-
-        self.currentLocation = 5;
-
-    } else if(CGRectContainsPoint(self.bottomLeft.frame, point)){
-
-        self.currentLocation = 6;
-
-    } else if(CGRectContainsPoint(self.bottomMiddle.frame, point)){
-
-        self.currentLocation = 7;
-
-    } else if(CGRectContainsPoint(self.bottomRight.frame, point)){
-
-        self.currentLocation = 8;
-
-    }
-}
-
 - (void) markPlayerTap {
-    NSString *mark = @"";
+
     if (self.isPlayerOne == YES){
-        mark = @"x";
+        self.mark = @"x";
     } else {
-        mark = @"o";
+        self.mark = @"o";
     }
 
-    for (int i=0;i<9;i++){
-        if(i == self.currentLocation)
-        {
-            [self makeMagicHappen:[self.gridLayout objectAtIndex:i] mark:mark];
-        }
-    }
+    [self makeMagicHappen:[self.gridLayout objectAtIndex:self.currentLocation]];
 
     if (!self.isValidMark){
+        [self addPlayerMove];
+        [self checkForWinner];
+        if (self.isWinner)
+            [self alertWinner];
         self.isPlayerOne ^= YES;
         self.isValidMark = YES;
     }
-
 }
 
 
--(void) makeMagicHappen : (UILabel *) label  mark: (NSString *) mark{
+-(void) makeMagicHappen : (UILabel *) label{
 
     if (!([label.text containsString:@"x"] || [label.text containsString:@"o"])){
-        label.text = mark;
+        label.text = self.mark;
         self.isValidMark = NO;
     }
 }
@@ -159,6 +105,8 @@
         label.text = @"";
     }
 
+    [self.playerOneMoves removeAllObjects];
+    [self.playerTwoMoves removeAllObjects];
     self.isPlayerOne = YES;
     self.isValidMark = YES;
     self.isWinner = NO;
@@ -166,31 +114,38 @@
 }
 
 //Brute Force method
-
 - (void) checkForWinner{
-    NSString *currentMark = @"o";
-    if (self.isPlayerOne == NO)
-        currentMark = @"x";
-
-    if(([[self.gridLayout objectAtIndex:0] text] == currentMark) && ([[self.gridLayout objectAtIndex:4] text] == currentMark) && ([[self.gridLayout objectAtIndex:8] text] == currentMark)){
-        self.isWinner = YES;
-    }else if(([[self.gridLayout objectAtIndex:6] text] == currentMark) && ([[self.gridLayout objectAtIndex:4] text] == currentMark) && ([[self.gridLayout objectAtIndex:2] text] == currentMark)){
-        self.isWinner = YES;
-    }else if(([[self.gridLayout objectAtIndex:6] text] == currentMark) && ([[self.gridLayout objectAtIndex:7] text] == currentMark) && ([[self.gridLayout objectAtIndex:8] text] == currentMark)){
-        self.isWinner = YES;
-    }else if(([[self.gridLayout objectAtIndex:0] text] == currentMark) && ([[self.gridLayout objectAtIndex:1] text] == currentMark) && ([[self.gridLayout objectAtIndex:2] text] == currentMark)){
-        self.isWinner = YES;
-    } else if(([[self.gridLayout objectAtIndex:2] text] == currentMark) && ([[self.gridLayout objectAtIndex:5] text] == currentMark) && ([[self.gridLayout objectAtIndex:8] text] == currentMark)){
-        self.isWinner = YES;
-    }else if(([[self.gridLayout objectAtIndex:0] text] == currentMark) && ([[self.gridLayout objectAtIndex:3] text] == currentMark) && ([[self.gridLayout objectAtIndex:6] text] == currentMark)){
-        self.isWinner = YES;
-    }else if(([[self.gridLayout objectAtIndex:1] text] == currentMark) && ([[self.gridLayout objectAtIndex:4] text] == currentMark) && ([[self.gridLayout objectAtIndex:7] text] == currentMark)){
-        self.isWinner = YES;
-    }else if(([[self.gridLayout objectAtIndex:3] text] == currentMark) && ([[self.gridLayout objectAtIndex:4] text] == currentMark) && ([[self.gridLayout objectAtIndex:5] text] == currentMark)){
-        self.isWinner = YES;
+    
+    if(self.isPlayerOne == YES) {
+        if([self.answerKey containsObject:self.playerOneMoves])
+            self.isWinner = YES;
+    } else {
+        if([self.answerKey containsObject:self.playerTwoMoves])
+            self.isWinner = YES;
     }
+}
+
+- (void) addPlayerMove{
+    if (self.isPlayerOne)
+        [self.playerOneMoves addObject:[NSNumber numberWithInt:self.currentLocation]];
+    else
+        [self.playerTwoMoves addObject:[NSNumber numberWithInt:self.currentLocation]];
 
 }
 
+-(void) alertWinner{
+    NSString *playerString = @"Player 1";
+    if (!self.isPlayerOne)
+        playerString = @"Player 2";
 
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"You're a Winner!" message:[playerString stringByAppendingString:@" won!"] preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction *okButton = [UIAlertAction actionWithTitle:@"YESSS!" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            [self resetAction];
+        }];
+
+    [alert addAction:okButton];
+    [self presentViewController:alert animated:YES completion:nil];
+
+}
 @end
